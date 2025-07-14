914990cdcb78a58fe6d42ff68cb97ffe8e0cac24
Jay Phillip Hernandez <jayhernandez3@my.unt.edu>
2025-07-14 00:21:15 +0000

    Created cargo and added code that returns size of a file path

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,1 +1,36 @@
 // update this file with your own tests
+use std::env;
+
+enum FileSize {
+    Bytes(u64),
+    Kilobytes(f64),
+    Megabytes(f64),
+    Gigabytes(f64),
+}
+
+fn format_size(size: u64) -> String {
+    let filesize = match size {
+        0..=999 => FileSize::Bytes(size),
+        1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
+        1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
+        _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+    };
+
+    match filesize {
+        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
+        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
+        FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
+        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+    }
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the size that was used to call the program. Must use quotes to
+    // read this as a single argument
+    println!("Size is: {}.", args[0]);
+    let result = format_size(6888837399);
+    println!("{}", result)
+}
